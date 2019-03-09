f754186e3a3e74d311dfc054cadfb51793135ec7
Nadine Adel <nadine.adel@robustastudio.com>
2019-03-09 22:51:02 +0200

    config: add less loader for antd and babel imports
    use Babel imports  to import automatically to antd component its css.

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,29 @@
 /* config-overrides.js */
-const { override, addBabelPlugins, addWebpackAlias } = require('customize-cra');
+const {
+  override,
+  addBabelPlugins,
+  addWebpackAlias,
+  fixBabelImports,
+  addLessLoader,
+} = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
-  ...addBabelPlugins('@babel/plugin-proposal-optional-chaining'),
+  ...addBabelPlugins('@babel/plugin-proposal-optional-chaining', 'emotion'),
   addWebpackAlias({
     '@components': path.resolve(__dirname, 'src/components'),
     '@utilities': path.resolve(__dirname, 'src/utilities'),
     '@scenes': path.resolve(__dirname, 'src/scenes'),
     '@services': path.resolve(__dirname, 'src/services'),
+  }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    // eg for modifying variables. refer to https://ant.design/docs/react/customize-theme#header
+    // modifyVars: { '@primary-color': '#ff0000' },
   })
 );
